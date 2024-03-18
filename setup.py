cde7e86e3d230ad816c9cab5fb2298eb92db89c3
amine0110 <mokhtari.amine1996@gmail.com>
2024-03-18 13:34:13 +0100

    updated numpy and tensorflow versions

--- setup.py
+++ setup.py
@@ -70,12 +70,12 @@ install_requires_projects = [
 
 install_requires_core = [
     "absl-py>=1.0.0",
-    "numpy>=1.12",
+    "numpy==1.26.0",
     "jax>=0.4.3",
     "jaxlib>=0.4.3",
     "flax>=0.4.0",
     "ml-collections>=0.1.1",
-    "tensorflow>=2.7",
+    "tensorflow==2.13",
     "tensorflow-addons>=0.15.0",
     "immutabledict>=2.2.1",
     "clu>=0.0.6",
